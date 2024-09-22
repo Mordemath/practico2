8b9be28f2ccad2302d8a15105fb79ac2c1bdeea3
Mordemath <starlightmatias7w7@gmail.com>
2024-09-22 01:52:41 -0300

    import y fctn pausa

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import Scannf from 'prompt-sync';
+import chalk from './node_modules/chalk/source/index';
 import { tarea } from './Tarea';
 let hayTareas: boolean = false;
-let tareas: tarea[];
+export let tareas: tarea[];
+let aux: tarea;

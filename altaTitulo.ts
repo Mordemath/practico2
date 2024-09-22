1145826bf98a2e0dce0b2ec5c03ba1c4d19932f8
Mordemath <starlightmatias7w7@gmail.com>
2024-09-22 19:33:21 -0300

    En proceso

---
+++ altaTitulo.ts
@@ -0,0 +1,28 @@
+import pausa from "./pausa"
+import prompt from "prompt-sync";
+const Scannf = prompt();
+//CargarTitulo*************************************************************************************************************************
+export default function CargarTitulo(titulo: string, f: string) {
+    console.clear();
+    do {
+        console.log(`[] Ingresa el titulo o solo [Enter] para cancelar.\n`);
+        titulo = Scannf(`>`);
+        if (titulo == " " || titulo == "  " || titulo == "   ") {
+            console.log("Eltitulo no puede estar vacío.\nVuelva a intentarlo.");
+            pausa();
+            console.clear();
+        }
+        if (titulo.length > 100) {
+            console.log("El titulo no puede superar los 100 caracteres.\nVuelva a intentarlo.");
+            pausa();
+            console.clear();
+        }
+    } while (titulo == " " || titulo == "  " || titulo == "   " || titulo.lenght > 100);
+    if (titulo == "") {
+        return `-1`;
+    }
+    else {
+        console.clear();
+        return titulo;
+    }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,55 @@
-import Scannf from 'prompt-sync';
+import * as am from './am';
+import prompt from 'prompt-sync';
 import chalk from './node_modules/chalk/source/index';
 import { tarea } from './Tarea';
-let hayTareas: boolean = false;
+import Pausa from './pausa'
+let Scannf = prompt();
 export let tareas: tarea[];
 let aux: tarea;
+let op: string | number = `1`;
+let hayTareas: boolean = false;
+while (op != `0`) {
+    console.log(chalk.blueBright("Hola Olivia\n¿Qué deseas hacer?\n"));
+    console.log("[1] Ver mis tareas.\n[2] Buscar una tarea.\n[3] Agregar una tarea.\n[0] Salir");
+    op = Scannf(`Opción: `);
+
+    switch (op) {
+        case `0`:
+            console.log(chalk.blueBright(`Hasta la proxima Olivia...`));
+            break;
+        case `1`:
+            if (hayTareas) {
+                MenuVer.MenuVer();
+            }
+            else {
+                console.log(chalk.redBright(`No hay tareas aún...`));
+            }
+            break;
+        case `2`:
+            if (hayTareas) {
+                MenuVer.BuscarPor(tareas, `1`, 0);
+            }
+            else {
+                console.log(chalk.redBright(`No hay tareas aún...`));
+            }
+            break;
+        default:
+            console.log(chalk.redBright(`Opción invalida...`));
+            op = -1;
+            break;
+        case `3`:
+            aux = am.AgregarTarea(`0`, false);
+            if (aux.Ttitulo == ` `) {
+                console.log(chalk.redBright(`Cancelado...`));
+            }
+            else {
+                tareas.push(aux);
+                console.log(chalk.greenBright(`¡Datos guardados!`));
+                MenuVer.VerDetalles(tareas.length - 1, tareas);
+                hayTareas = true;
+            }
+            break;
+    }
+    Pausa();
+    console.clear();
+}
